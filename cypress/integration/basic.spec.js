b635bb95ba594944f8dfc369ef76942fb4a0b9a7
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2022-09-01 17:43:28 -0300

    Debug and Pause

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,19 +1,24 @@
 /// <reference types="cypress" />
 
 describe("Cypress basics", () => {
-  it("Should visit a page and assert title", () => {
+  it.only("Should visit a page and assert title", () => {
     cy.visit("https://wcaquino.me/cypress/componentes.html");
+
+    //Pausa a execução nesta linha. 
+    cy.pause()
 
     //.should tenta repetidas vezes com o parametro passado até passar, ou cair no timeout
     cy.title().should("be.equal", "Campo de Treinamento");
-    cy.title().should("contain", "Campo");
+
+    // debug exibe no console informações
+    cy.title().should("contain", "Campo").debug();
 
     cy.title()
       .should("be.equal", "Campo de Treinamento")
       .and("contain", "Campo");
   });
 
-  it.only("Should find and interact with an element", () => {
+  it("Should find and interact with an element", () => {
     cy.visit("https://wcaquino.me/cypress/componentes.html");
     
     cy.get("#buttonSimple")
