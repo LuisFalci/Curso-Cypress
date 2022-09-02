f9b55d7f98d9bef5af95a148a80ae6da381aa284
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2022-09-01 21:04:35 -0300

    Links and reload

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -14,4 +14,15 @@ describe("Work with basic elements", () => {
       "Cuidado onde clica, muitas armadilhas..."
     );
   });
+
+  it.only("Links", () => {
+    cy.visit("https://wcaquino.me/cypress/componentes.html");
+    cy.get('[href="#"]').click();
+    cy.get("#resultado").should("have.text", "Voltou!");
+
+    cy.reload();
+    cy.get("#resultado").should("have.not.text", "Voltou!");
+    cy.contains("Voltar").click();
+    cy.get("#resultado").should("have.text", "Voltou!");
+  });
 });
