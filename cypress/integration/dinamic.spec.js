ef0ace8fcea04d9152720b099b6218d4345aa9f7
Luis Gustavo Furtuozo Falci <78694561+LuisFalci@users.noreply.github.com>
2022-09-05 23:50:58 -0300

    Create dinamic.spec.js

---
+++ cypress/integration/dinamic.spec.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+describe('Dinamic tests', () => {
+    beforeEach(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+    })
+
+    const foods = ['Carne', 'Frango', 'Pizza', 'Vegetariano']
+    //Rodamos um teste para cada elemento do array
+    foods.forEach(food => {
+        it(`Cadastro com a comida ${food}`, () => {
+            cy.get('#formNome').type('Usuario')
+            cy.get('#formSobrenome').type('Qualquer')
+            cy.get(`[name=formSexo][value=F]`).click()
+            cy.xpath(`//label[contains(., '${food}')]/preceding-sibling::input`).click()
+            cy.get('#formEscolaridade').select('Doutorado')
+            cy.get('#formEsportes').select('Corrida')
+            cy.get('#formCadastrar').click()
+            cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+        })
+    })
+
+    it('Deve selecionar todos usando o each', () => {
+        cy.get('#formNome').type('Usuario')
+        cy.get('#formSobrenome').type('Qualquer')
+        cy.get(`[name=formSexo][value=F]`).click()
+
+        //para cada elento encontrado, realizar um click
+        cy.get('[name=formComidaFavorita]').each($el => {
+            // $el.click()
+            //se for diferente de vegetariano ele clica
+            if ($el.val() !== 'vegetariano')
+                cy.wrap($el).click()
+        })
+
+        cy.get('#formEscolaridade').select('Doutorado')
+        cy.get('#formEsportes').select('Corrida')
+        cy.get('#formCadastrar').click()
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+        // cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
+    })
+
+})
